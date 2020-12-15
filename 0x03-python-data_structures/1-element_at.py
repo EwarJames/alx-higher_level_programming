b11a42abe2bf7a9e302caed091c91334cdb22a38
EwarJames <ewaarjames@gmail.com>
2020-12-15 14:18:48 +0000

    Retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,7 @@
 
 
 def element_at(my_list, idx):
-    for idx in range(len(my_list)):
-        if idx < 0 and idx > range(len(my_list)):
+        if idx < 0 and idx > range(len(my_list) - 1):
             return None
         else:
-            return len(my_list) - 1
+            return my_list[idx]
