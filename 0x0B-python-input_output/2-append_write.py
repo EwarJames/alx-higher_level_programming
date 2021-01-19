c261fd7b1c49763f6c7a07b0c4ff5cbdb04da524
EwarJames <ewaarjames@gmail.com>
2021-01-19 16:39:11 +0000

    Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define a function to append a string."""
+
+
+def append_write(filename="", text=""):
+    """
+    Function that appends a srting at the end of a text.
+
+    Args:
+       filename (str): file to be appended.
+       text (str): text to append
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.append(text)
