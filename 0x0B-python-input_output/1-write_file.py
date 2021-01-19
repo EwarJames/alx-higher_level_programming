0a9439ed282f46376adc2d364afae7ec93c7f77a
EwarJames <ewaarjames@gmail.com>
2021-01-19 16:22:00 +0000

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define a function that writes to a textfile."""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a textfile encoded in utf-8.
+
+    Args:
+        filename (str): The name of the file.
+        text (str): text to write
+
+    Return:
+        Number of characters written.
+    """
+    def write_file(filename="", text=""):
+        """Writes to a file"""
+        with open(filename, "w", encoding="utf-8") as f:
+            return f.write(text)
