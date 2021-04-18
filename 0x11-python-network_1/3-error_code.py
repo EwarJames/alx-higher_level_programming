348df5c948feb21147bb5d44e92f84927a3338c4
EwarJames <ewaarjames@gmail.com>
2021-04-18 12:30:35 +0000

    Error code correction

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""
-Sends a request to the URL and displays the body of the response.
-
+"""Sends a request to a given URL and displays the response body.
 Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
 """
 import sys
-import urllib.request
 import urllib.error
+import urllib.request
 
 
-__name__ == "__main__":
+if __name__ == "__main__":
     url = sys.argv[1]
 
-    req = urllib.request.Request(url)
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(req) as response:
-            print(response.read().decode(ascii))
-    except urllib.error.HTTPerror as e:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
