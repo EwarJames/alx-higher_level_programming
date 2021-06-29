a152af6eb4a63f420b4a988f6a7b6d92e72bea4e
EwarJames <ewaarjames@gmail.com>
2021-06-29 18:15:30 +0000

    Script that converts a databse to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- Converts hbtn_0c_0 database to utf8
 USE `hbtn_0c_0`
 ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8_mb4_unicode_ci; 
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8_mb4unicode_ci; 
