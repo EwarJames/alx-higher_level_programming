b7d7a50a21283072cbd55c8b54d7b9dbb8520502
EwarJames <ewaarjames@gmail.com>
2021-06-30 12:28:12 +0000

    Script that lists all privileges for user_0d_1 and user_0d_2

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Lists privileges of Mysql users user_0d_1 and user_0d_2
 -- On a localhost
-SHOW GRANTS FOR 'user_od_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
